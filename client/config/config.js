ab1cda45007d3f23134de39ad30832d80ad61b9d
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-27 14:23:08 -0700

    regular checkin

--- client/config/config.js
+++ client/config/config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('angBank')
+angular.module('gradetest')
 .config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/');
 
@@ -8,8 +8,7 @@ angular.module('angBank')
   .state('home', {url: '/', templateUrl: '/views/general/home.html'})
   .state('about', {url: '/about', templateUrl: '/views/general/about.html'})
   .state('faq', {url: '/faq', templateUrl: '/views/general/faq.html'})
-  .state('contact', {url: '/contact', templateUrl: '/views/general/contact.html'})
   .state('register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
   .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
-  .state('accounts', {url: '/accounts', templateUrl: '/views/accounts/accounts.html', controller: 'AccountsCtrl'});
+  .state('grades', {url: '/grades', templateUrl: '/views/grades/grades.html', controller: 'GradesCtrl'});
 });
--- client/config/constants.js
+++ client/config/constants.js
@@ -1,4 +1,4 @@
 'use strict';
 
 angular.module('angBank')
-.constant('firebaseUrl', 'https://angularbank.firebaseio.com/');
+.constant('firebaseUrl', 'https://gradetest.firebaseio.com/');
