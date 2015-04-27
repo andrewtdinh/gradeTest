5eb7477556f15bb2d7845dba75df449f1b77ae81
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-27 14:16:13 -0700

    initial

---
+++ client/config/constants.js
@@ -0,0 +1,4 @@
+'use strict';
+
+angular.module('angBank')
+.constant('firebaseUrl', 'https://angularbank.firebaseio.com/');
---
+++ client/models/account.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('angBank')
+.factory('Account', function($rootScope, $firebaseObject, $firebaseArray, $window){
+  var fbUser;
+  var afUser;
+
+  function Account(){
+  }
+
+  Account.init = function(){
+    fbUser = $rootScope.fbRoot.child('users/' + $rootScope.activeUser.uid);
+    afUser = $firebaseObject(fbUser);
+    return afUser;
+  };
+
+  Account.delTransaction = function(tx, index){
+    var fbTransactions = fbUser.child('accounts/' + tx.type);
+    var afTransactions = $firebaseArray(fbTransactions);
+    afTransactions.$loaded().then(function(){
+      var foundTx = afTransactions[index];
+      afTransactions.$remove(foundTx);
+    });
+  };
+
+  Account.addTransaction = function(name, tx){
+    var transaction = angular.copy(tx);
+    transaction.date = transaction.date.getTime();
+    transaction.createdAt = $window.Firebase.ServerValue.TIMESTAMP;
+    transaction.name = name;
+    var fbTransactions = fbUser.child('accounts/' + tx.type);
+    var afTransactions = $firebaseArray(fbTransactions);
+    afTransactions.$add(transaction);
+  };
+
+  Account.add = function(name){
+    var names = afUser.names ? afUser.names.split(',') : [];
+    names.push(name);
+
+    afUser.names = names.join(',');
+    return afUser.$save();
+  };
+
+  return Account;
+});
